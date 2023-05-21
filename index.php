569e4ba135e3f9b6273fb15bdcf1c0a04a6a7d6e
Riquelme <132308905+JoaoRiquelme115@users.noreply.github.com>
2023-05-21 18:46:54 -0300

    Título e Barra de Busca

--- index.php
+++ index.php
@@ -15,5 +15,25 @@
             <button class="botao2">Seja um Profissional</button>
         </div>
     </header>
+
+    <div class="menu">
+        <img src="imagens/saude.png" alt="saúde">
+        <img src="imagens/carro.png" alt="carro">
+        <img src="imagens/eventos.png" alt="eventos">
+        <img src="imagens/aula.png" alt="aula">
+    </div>
+
+    <div class="titulo">
+        <h1>Conectando quem precisa com quem sabe fazer</h1>
+        <p>Fale o que precisa, receba até 4 orçamentos, escolha o melhor</p>
+    </div>
+
+    <div class="busca">
+        <img src="imagens/lupa.png" alt="ícone de pesquisa">
+        <form action="detalhe.php" method="GET">
+            <input type="search" name="busca" placeholder="Digite aqui sua pesquisa">
+            <button>Buscar</button>
+        </form>
+    </div>
 </body>
 </html>
