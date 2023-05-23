d54996a08233534adac0acfe77a866a81267b8b2
Riquelme <132308905+JoaoRiquelme115@users.noreply.github.com>
2023-05-23 09:13:04 -0300

    Adicionando os dados
    Dados dos serviços de Saúde, Autos, Eventos e Aulas

--- dados.php
+++ dados.php
@@ -1,1 +1,93 @@
 <!--Todos os dados dos serviços do site, arrays-->
+<?php
+$servicos = [
+    "Saúde" => [
+        [
+            "nome" => "Nutricionista",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Quiropraxia",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Dentista",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Pscologia",
+            "foto" => "",
+            "descricao" => ""
+        ]
+    ],
+    "Autos" => [
+        [
+            "nome" => "Borracharia",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Mêcanica",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Pintura",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Guincho",
+            "foto" => "",
+            "descricao" => ""
+        ]
+    ],
+    "Eventos" => [
+        [
+            "nome" => "Fotografia",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Decoração",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Segurança",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Recepcionista",
+            "foto" => "",
+            "descricao" => ""
+        ]
+    ],
+    "Aula" => [
+        [
+            "nome" => "Informática",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Idiomas",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Pré-Vestibular",
+            "foto" => "",
+            "descricao" => ""
+        ],
+        [
+            "nome" => "Reforço",
+            "foto" => "",
+            "descricao" => ""
+        ]
+    ]
+];
+?>
