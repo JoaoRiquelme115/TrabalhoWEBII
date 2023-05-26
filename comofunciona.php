7cd039cbdc191f103eca2d7395bcdd9a4766a544
Ludmylla Santana <132173977+santanaLudmylla@users.noreply.github.com>
2023-05-26 20:03:55 -0300

    Update comofunciona.php

--- comofunciona.php
+++ comofunciona.php
@@ -1,1 +1,123 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Contratação de Serviços</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="estilo2.css" rel="stylesheet">
+</head>
+<header>
+        <img src="imagens/Cs3.png" alt="Logo do site">
+        <div>
+            
+            <button class="botao1"><a href="index.php">Pagina inicial</a></button>
+            <button class="botao2"><a href="profissionais.php">Seja um Profissional</a></button>
+        </div>
+    </header>
+<div class="menu">
+        <a href="detalhe.php"><img src="imagens/saude.png" alt="saúde"></a>
+        <a href="detalhe.php"><img src="imagens/carro.png" alt="carro"></a>
+        <a href="detalhe.php"><img src="imagens/eventos.png" alt="eventos"></a>
+        <a href="detalhe.php"><img src="imagens/aula.png" alt="aula"></a>
+    </div>
+<body>
+<div class="titulo2"><h1>O que é o Cs group?</h1>
+<h3>Cs group é a maior plataforma de contratação de serviços do Brasil. Conectamos Profissionais de todo o Brasil com pessoas solicitando serviço, atendendo com qualidade, facilidade e rapidez todos os tipos de necessidade.</h3>
+</div>
+
+<img src="imagens/CS2.png" alt="Logo do site">
+
+
+<div class="titulo2"><h1>Como funciona?</h1>
+<h3>É rapido e facil! Faça seu pedido, nossos profissionais entram em contato com você enviando até 4 orçamentos, você ira escolher o melhor e negociar direto com eles.</h3>
+</div>
+
+<img src="imagens/ruu2.jpg" alt="Imagem de icons">
+
+
+<div class="titulo2"><h1>Como o Cs Group funciona para o Profissional?</h1>
+<h3> Para o funcionario seu emprego já ta na mão! Pedidos de serviços chegando toda hora no seu celular,você ve todos os pedidos de graça, e pode invistir apenas nos que gostar. Vale lembrar que 100% do valor do serviço é seu. E sem mensalidade!
+</h3>
+</div>
+
+<img src="imagens/trabalhador2.png" alt="Imagem de um trabalhador">
+
+
+<footer class="footer"><!--footer-marca a área inferior, normalmente conhecida como rodapé-->
+  <div class="footer__addr">
+    <h1 class="footer__logo"></h1>
+        
+    <h2>Atendimento ao cliente</h2>
+    
+    <address>
+      (77) 40028922<br>
+           <a class="footer__btn" href=""> suportecsgroup@gmail.com.br
+</a>
+    </address>
+  </div>
+  
+  <ul class="footer__nav">
+    <li class="nav__item">
+      <h2 class="nav__title">  Media</h2>
+
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Em todas as redes sociais</a>
+        </li>
+        <li>
+          <a href="#">@Csgroup_Brasil</a>
+        </li>
+      </ul>
+    </li>
+    
+    <li class="nav_item nav_item--extra">
+      <h2 class="nav__title">Website Designer by</h2>
+      <ul class="nav_ul nav_ul--extra">
+             <li>
+              <p>Elayne</p>
+           </li>
+          <li>
+             <p>João Riquelme</p>
+          </li>
+          <li>
+            <p>Ludmylla</p>
+          </li>
+          <li>
+            <p>Rayka</p>
+
+        </li>
+      </ul>
+    </li>
+      
+    
+    </li>
+    
+    <li class="nav__item">
+      <h2 class="nav__title">Legal</h2>
+      
+      <ul class="nav__ul">
+        <li>
+          <a href="#">Politica de Privacidade</a>
+        </li>
+        
+        <li>
+          <a href="#">Termos de uso</a>
+        </li>
+        
+      </ul>
+    </li>
+  </ul>
+  
+  <div class="legal">
+    <p>&copy; 2022  All rights reserved.</p>
+    
+    <div class="legal__links">
+      <span>Made with <span class="heart">♥
+    </div>
+  </div>
+</footer>
+
+  </body>
+</html>
 
